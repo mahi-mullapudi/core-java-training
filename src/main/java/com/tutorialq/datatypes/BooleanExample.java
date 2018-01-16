a4302af11667b6f773831f073f3e5abec3653603
Mahidhar Mullapudi <mahi.mullapudi@gmail.com>
2018-01-16 16:40:35 -0500

    Initial Commit

---
+++ src/main/java/com/tutorialq/datatypes/BooleanExample.java
@@ -0,0 +1,11 @@
+package com.tutorialq.datatypes;
+
+public class BooleanExample {
+
+    public static void main(String[] args) {
+        boolean flag = false;
+        System.out.println("Inside main method of BooleanExample --> flag: " + flag);
+        flag = true;
+        System.out.println("Updated the value --> flag: " + flag);
+    }
+}
